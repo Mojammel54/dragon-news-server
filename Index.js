65b03bba9e425309b8171970ddcb06637e8e085a
JIBON <jibon2230@gmail.com>
2022-10-22 10:11:12 +0600

    vercel.json file added

--- Index.js
+++ Index.js
@@ -8,7 +8,7 @@ app.use(cors())
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
-
+const news = require('./data/news.json')
 app.get('/', (req, res) => {
 
     res.send('news Api Running');
@@ -19,6 +19,37 @@ app.get('/', (req, res) => {
 app.get('/news-categories', (req, res) => {
 
     res.send(categories)
+
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+
+    const selectedNews = news.find(n => n._id === id)
+
+    res.send(selectedNews)
+
+})
+app.get('/news', (req, res) => {
+
+
+    res.send(news)
+
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id
+    if (id === '08') {
+        res.send(news)
+
+    }
+    else {
+
+        const category_news = news.filter(n => n.category_id === id);
+
+        res.send(category_news)
+
+    }
+
 
 })
 
